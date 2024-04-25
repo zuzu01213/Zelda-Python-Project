553877b091e7a2df3bc0f33e16bb463168ae2702
azzza <139770919+zuzu01213@users.noreply.github.com>
2024-04-25 06:05:59 -0400

    ok

--- level.py
+++ level.py
@@ -64,6 +64,7 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.offset = pygame.math.Vector2()
 
 		# creating the floor
+  # creating the floor
 		self.floor_surf = pygame.image.load('graphics/tilemap/ground.png').convert()
 		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
